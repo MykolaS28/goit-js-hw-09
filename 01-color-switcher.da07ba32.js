431badfb576ac04ecdcfbf0acf218a1df1503c12
MykolaS28 <127856142+MykolaS28@users.noreply.github.com>
2023-08-09 20:22:27 +0000

    Deploying to gh-pages from @ MykolaS28/goit-js-hw-09@e8c931d83f96ea65349258e2da6e85f5435a5c3a 🚀

--- 01-color-switcher.da07ba32.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,a=null;t.addEventListener("click",(function(){a=setInterval((function(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.da07ba32.js.map
